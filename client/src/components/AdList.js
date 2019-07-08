a7c86779d3f96082d5c92158ee3de317e2fcc361
Marcus Lundgren <lundgren90@linuxmail.org>
2019-07-08 21:55:38 +0200

    prepare for delpoyment2

--- client/src/components/AdList.js
+++ client/src/components/AdList.js
@@ -23,7 +23,7 @@ const AdList = ({ getPublicAds, posts: { ads, loading } }) => {
           {!loading &&
             ads &&
             ads.map(ad => (
-              <div class="single-ad-container">
+              <div key={ad._id} class="single-ad-container">
                 <Link className="link" to={`/ad/${ad._id}`}>
                   <div class="single-ad">
                     <div class="job-title">{ad.title}</div>
